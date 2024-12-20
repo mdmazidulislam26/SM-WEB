2f333c7503d9448b98370a1b60b31c92c2e49988
Md Mazidul Idslam <100226255+mdmazidulislam26@users.noreply.github.com>
2024-12-20 17:00:41 +0600

    Update JwtConstant.java

--- src/main/java/com/mazid/config/JwtConstant.java
+++ src/main/java/com/mazid/config/JwtConstant.java
@@ -2,5 +2,5 @@ package com.mazid.config;
 
 public class JwtConstant {
     public static String JWT_HEADER = "Authorization";
-    public static String SECRET_KEY ="2qw3e4drf5vy7alwejfoai30934wir04";
+    public static String SECRET_KEY ="2qw3e4drf5vy7alwej09i8ju7hy";
 }
