faa00c28f3cc628888a3098cdc177d4d5299b0b5
Md Mazidul Idslam <100226255+mdmazidulislam26@users.noreply.github.com>
2024-12-22 21:46:03 +0600

    Update ReelsController.java

--- src/main/java/com/mazid/controller/ReelsController.java
+++ src/main/java/com/mazid/controller/ReelsController.java
@@ -11,33 +11,38 @@ import java.util.List;
 
 @RestController
 public class ReelsController {
+
     @Autowired
     private ReelsService reelsService;
 
     @Autowired
     private UserService userService;
 
+    // Endpoint to create a new reel for a user
     @PostMapping("/api/reels")
-    public Reels createReels(@RequestBody Reels reel, @RequestHeader("Authorization")String jwt){
-
+    public Reels createReels(@RequestBody Reels reel, @RequestHeader("Authorization") String jwt) {
+        // Find the user from the JWT token
         User reqUser = userService.findUserByJwt(jwt);
 
-        Reels createdReels = reelsService.createReel(reel,reqUser);
+        // Create a new reel for the user
+        Reels createdReels = reelsService.createReel(reel, reqUser);
 
         return createdReels;
     }
 
+    // Endpoint to get all reels
     @GetMapping("/api/reels")
-    public List<Reels> findAllReels(){
-
+    public List<Reels> findAllReels() {
+        // Retrieve all reels
         List<Reels> reels = reelsService.findAllReels();
 
         return reels;
     }
 
+    // Endpoint to get all reels for a specific user
     @GetMapping("/api/reels/user/{userId}")
     public List<Reels> findUserReels(@PathVariable Integer userId) throws Exception {
-
+        // Retrieve all reels for the user by userId
         List<Reels> reels = reelsService.findUsersReel(userId);
 
         return reels;
