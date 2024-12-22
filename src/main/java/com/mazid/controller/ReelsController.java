e6c51fdcd7ce4774faaa99b38b1424ec0dfea374
Md Mazidul Idslam <100226255+mdmazidulislam26@users.noreply.github.com>
2024-12-22 21:46:20 +0600

    Update ReelsController.java

--- src/main/java/com/mazid/controller/ReelsController.java
+++ src/main/java/com/mazid/controller/ReelsController.java
@@ -1,8 +1,8 @@
 package com.mazid.controller;
 
-import com.mazid.models.Reels;
+import com.mazid.models.Story;
 import com.mazid.models.User;
-import com.mazid.service.ReelsService;
+import com.mazid.service.StoryService;
 import com.mazid.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -10,41 +10,34 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-public class ReelsController {
+public class StoryController {
 
     @Autowired
-    private ReelsService reelsService;
-
+    private StoryService storyService;
     @Autowired
     private UserService userService;
 
-    // Endpoint to create a new reel for a user
-    @PostMapping("/api/reels")
-    public Reels createReels(@RequestBody Reels reel, @RequestHeader("Authorization") String jwt) {
+    // Endpoint to create a new story for a user
+    @PostMapping("/api/story")
+    public Story createStory(@RequestBody Story story, @RequestHeader("Authorization") String jwt) {
         // Find the user from the JWT token
         User reqUser = userService.findUserByJwt(jwt);
 
-        // Create a new reel for the user
-        Reels createdReels = reelsService.createReel(reel, reqUser);
+        // Create a new story for the user
+        Story createdStory = storyService.createStory(story, reqUser);
 
-        return createdReels;
+        return createdStory;
     }
 
-    // Endpoint to get all reels
-    @GetMapping("/api/reels")
-    public List<Reels> findAllReels() {
-        // Retrieve all reels
-        List<Reels> reels = reelsService.findAllReels();
+    // Endpoint to get all stories of a specific user by userId
+    @GetMapping("/api/story/user/{userId}")
+    public List<Story> findUsersStory(@PathVariable Integer userId, @RequestHeader("Authorization") String jwt) throws Exception {
+        // Find the user from the JWT token
+        User reqUser = userService.findUserByJwt(jwt);
 
-        return reels;
-    }
+        // Retrieve all stories for the user by userId
+        List<Story> stories = storyService.findStoryByUserId(userId);
 
-    // Endpoint to get all reels for a specific user
-    @GetMapping("/api/reels/user/{userId}")
-    public List<Reels> findUserReels(@PathVariable Integer userId) throws Exception {
-        // Retrieve all reels for the user by userId
-        List<Reels> reels = reelsService.findUsersReel(userId);
-
-        return reels;
+        return stories;
     }
 }
