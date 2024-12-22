a2bd01f199aaf35420f9b7cde44cd9fbade4691e
Md Mazidul Idslam <100226255+mdmazidulislam26@users.noreply.github.com>
2024-12-22 21:39:51 +0600

    Update JwtConstant.java

--- src/main/java/com/mazid/config/JwtConstant.java
+++ src/main/java/com/mazid/config/JwtConstant.java
@@ -1,6 +1,11 @@
 package com.mazid.config;
 
+// Class to store JWT-related constants
 public class JwtConstant {
+
+    // Header key used for JWT authentication
     public static String JWT_HEADER = "Authorization";
-    public static String SECRET_KEY ="2qw3e4drf5vy7alwej09i8ju7hy";
+
+    // Secret key used for signing and verifying JWT tokens (should be stored securely)
+    public static String SECRET_KEY ="2qw3e4drf5vy7alwejfoai30934wir04";
 }
