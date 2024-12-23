1a4304041d5a77c1b17e49392005eeccda5a655b
Md Mazidul Idslam <100226255+mdmazidulislam26@users.noreply.github.com>
2024-12-23 22:22:37 +0600

    Update Comment.java

--- src/main/java/com/mazid/models/Comment.java
+++ src/main/java/com/mazid/models/Comment.java
@@ -6,22 +6,24 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+// @Entity annotation indicates that this class represents a database entity.
 @Entity
 public class Comment {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id // @Id annotation marks the primary key of the entity.
+    @GeneratedValue(strategy = GenerationType.AUTO)// @GeneratedValue strategy indicates that the id field will be auto-generated.
     private Integer id;
-
+    // Content of the comment, which is the text written by the user.
     private String content;
-    @ManyToOne
+    @ManyToOne// @ManyToOne indicates that each comment is associated with a single user (many comments can belong to one user).
     private User user;
+    // @ManyToMany indicates a many-to-many relationship between comments and users who liked the comment.
+    // Multiple users can like a single comment.
     @ManyToMany
     private List<User> liked = new ArrayList<>();
+    private LocalDateTime createdAt;    // Timestamp for when the comment was created.
 
-    private LocalDateTime createdAt;
-
-    public Comment(){}
-
+    public Comment(){}// Default constructor
+    // Parameterized constructor for initializing a Comment object.
     public Comment(Integer id, String content, User user, List<User> liked, LocalDateTime createdAt) {
         super();
         this.id = id;
@@ -30,7 +32,7 @@ public class Comment {
         this.liked = liked;
         this.createdAt = createdAt;
     }
-
+    // Getter and Setter methods for each field.
     public Integer getId() {
         return id;
     }
