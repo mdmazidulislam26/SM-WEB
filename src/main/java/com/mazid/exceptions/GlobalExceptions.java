4fa6509048c081c065d517dc98fa992e88a2e43d
Md Mazidul Idslam <100226255+mdmazidulislam26@users.noreply.github.com>
2024-12-23 22:19:03 +0600

    Update GlobalExceptions.java

--- src/main/java/com/mazid/exceptions/GlobalExceptions.java
+++ src/main/java/com/mazid/exceptions/GlobalExceptions.java
@@ -11,11 +11,14 @@ import java.time.LocalDateTime;
 @ControllerAdvice
 public class GlobalExceptions {
 
-
+    // This method handles all exceptions thrown in the application
+    // It returns a custom error response with a BAD_REQUEST status
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorDetails> otherExceptionHandler(Exception ex, WebRequest req){
+        // Creating an error details object with the exception message, description, and current timestamp
         ErrorDetails error = new ErrorDetails(ex.getMessage(), req.getDescription(false), LocalDateTime.now());
 
+        // Returning the error details inside a ResponseEntity with a BAD_REQUEST status code
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
