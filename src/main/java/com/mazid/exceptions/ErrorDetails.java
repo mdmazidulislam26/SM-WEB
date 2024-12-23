92304cf6f0813883ab4177665caf56571839d305
Md Mazidul Idslam <100226255+mdmazidulislam26@users.noreply.github.com>
2024-12-23 22:16:57 +0600

    Update ErrorDetails.java

--- src/main/java/com/mazid/exceptions/ErrorDetails.java
+++ src/main/java/com/mazid/exceptions/ErrorDetails.java
@@ -6,12 +6,18 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+// ErrorDetails class represents the structure of the error response to be sent to the client
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ErrorDetails {
 
+    // A message describing the error
     private String message;
+
+    // A specific error code or type
     private String error;
+
+    // Timestamp indicating when the error occurred
     private LocalDateTime timestamp;
 }
