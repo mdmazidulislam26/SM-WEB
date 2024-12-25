30df78f27768a42d89214a9e19a11a67ede1b2a7
Md Mazidul Idslam <100226255+mdmazidulislam26@users.noreply.github.com>
2024-12-25 21:45:30 +0600

    Update CommentRepository.java

--- src/main/java/com/mazid/repository/CommentRepository.java
+++ src/main/java/com/mazid/repository/CommentRepository.java
@@ -3,6 +3,9 @@ package com.mazid.repository;
 import com.mazid.models.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CommentRepository
-        extends JpaRepository<Comment,Integer> {
+// JpaRepository provides JPA related methods for working with Comment entity.
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
+    // JpaRepository automatically provides basic CRUD methods for the Comment entity.
+    // These include methods such as save(), findById(), findAll(), deleteById(), etc.
+    // No custom methods are needed here, but they can be added if required.
 }
