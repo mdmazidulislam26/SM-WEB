8df654a6bb82fb1e5379b3a6f77480af4d3beb34
Md Mazidul Idslam <100226255+mdmazidulislam26@users.noreply.github.com>
2024-12-25 21:45:55 +0600

    Update MessageRepository.java

--- src/main/java/com/mazid/repository/MessageRepository.java
+++ src/main/java/com/mazid/repository/MessageRepository.java
@@ -2,10 +2,12 @@ package com.mazid.repository;
 
 import com.mazid.models.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
+// JpaRepository provides JPA related methods for working with Message entity.
 public interface MessageRepository extends JpaRepository<Message, Integer> {
 
+    // Method to find all messages associated with a specific chat by its chat ID.
+    // This returns a list of messages for the specified chat.
     public List<Message> findByChatId(Integer chatId);
 }
