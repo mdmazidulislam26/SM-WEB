3c88661e840ce038eecdcdd4537592168ed481ac
Md Mazidul Idslam <100226255+mdmazidulislam26@users.noreply.github.com>
2024-12-25 21:47:11 +0600

    Update ReelsRepository.java

--- src/main/java/com/mazid/repository/ReelsRepository.java
+++ src/main/java/com/mazid/repository/ReelsRepository.java
@@ -2,9 +2,12 @@ package com.mazid.repository;
 
 import com.mazid.models.Reels;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
-public interface ReelsRepository extends JpaRepository<Reels,Integer> {
+// JpaRepository provides JPA related methods for working with Reels entity.
+public interface ReelsRepository extends JpaRepository<Reels, Integer> {
+
+    // Method to find all reels by a specific user's ID.
+    // This returns a list of reels associated with the given user.
     public List<Reels> findByUserId(Integer userId);
 }
