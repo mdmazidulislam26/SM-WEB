050aec3c614da1f77f9717986cc05d963b7f851a
Md Mazidul Idslam <100226255+mdmazidulislam26@users.noreply.github.com>
2024-12-25 21:46:27 +0600

    Update PostRepository.java

--- src/main/java/com/mazid/repository/PostRepository.java
+++ src/main/java/com/mazid/repository/PostRepository.java
@@ -3,11 +3,13 @@ package com.mazid.repository;
 import com.mazid.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
 import java.util.List;
 
-public interface PostRepository extends JpaRepository<Post,Integer> {
+// JpaRepository provides JPA related methods for working with Post entity.
+public interface PostRepository extends JpaRepository<Post, Integer> {
 
+    // Custom query to find posts by the user's ID.
+    // This query retrieves all posts created by a specific user.
     @Query("SELECT p FROM Post p WHERE p.user.id = :userId")
     List<Post> findPostByUserId(Integer userId);
 }
