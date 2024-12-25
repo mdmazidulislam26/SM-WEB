028349e136f91ec7a727e57eac7187e9cd7eca91
Md Mazidul Idslam <100226255+mdmazidulislam26@users.noreply.github.com>
2024-12-25 21:47:36 +0600

    Update StoryRepository.java

--- src/main/java/com/mazid/repository/StoryRepository.java
+++ src/main/java/com/mazid/repository/StoryRepository.java
@@ -2,10 +2,12 @@ package com.mazid.repository;
 
 import com.mazid.models.Story;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
-public interface StoryRepository extends JpaRepository<Story,Integer> {
+// JpaRepository provides JPA related methods for working with Story entity.
+public interface StoryRepository extends JpaRepository<Story, Integer> {
 
+    // Method to find all stories by a specific user's ID.
+    // This returns a list of stories associated with the given user.
     public List<Story> findByUserId(Integer userId);
 }
