3b4a3ed69319829f1e942b925975f392d60464aa
Md Mazidul Idslam <100226255+mdmazidulislam26@users.noreply.github.com>
2024-12-29 20:37:23 +0600

    Update CreateChatRequest.java

--- src/main/java/com/mazid/request/CreateChatRequest.java
+++ src/main/java/com/mazid/request/CreateChatRequest.java
@@ -2,8 +2,10 @@ package com.mazid.request;
 
 import lombok.Data;
 
+// CreateChatRequest class is used to capture the user ID for creating a chat
 @Data
 public class CreateChatRequest {
 
+    // ID of the user with whom the chat will be created
     private Integer userId;
 }
